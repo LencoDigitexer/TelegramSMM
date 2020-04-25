f8727bdb89d83b6852b831ee221c1652cd306d56
Илья Евсеенко <addminuser258@gmail.com>
2020-04-25 22:39:48 +0300

    Подключение переменного окружения

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+import os
+from dotenv import load_dotenv
+load_dotenv()
+telegram_bot_token = os.getenv("telegram_bot_token")
+
+
+bot = telebot.TeleBot(telegram_bot_token)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
